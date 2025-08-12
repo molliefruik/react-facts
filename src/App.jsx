28835a66ede72906dced2c673749c04ebf078f60
Tyler Fruik <tylerfruik@Tylers-MBP.attlocal.net>
2025-08-12 18:50:43 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Main from "/src/components/main"
+import Navbar from "/src/components/Navbar"
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Main />
+    </>
+  )
+}
+
