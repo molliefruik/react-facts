74443aca70134b22c7924462ccfcf7e3583b42bd
Tyler Fruik <tylerfruik@Tylers-MBP.attlocal.net>
2025-08-12 18:55:41 -0500

    fix typo in img

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ export default function Navbar() {
     return (
       <header>
         <nav>
-          <img src="/react-logo.png" alt="React logo" />
+          <img src="/react-logo.svg" alt="React logo" />
           <span className="react-logo-text">ReactFacts</span>
         </nav>
       </header>
