79a8e38c71596c1a16451cbdbbbb3fa9ca858d84
Tyler Fruik <tylerfruik@Tylers-MBP.attlocal.net>
2025-08-12 19:00:16 -0500

    update path

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ export default function Navbar() {
     return (
       <header>
         <nav>
-          <img src="../src/assets/react.svg" alt="React logo" />
+          <img src="../assets/react.svg" alt="React logo" />
           <span className="react-logo-text">ReactFacts</span>
         </nav>
       </header>
